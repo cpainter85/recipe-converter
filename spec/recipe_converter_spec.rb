08c0e78e488a4ffed804c5686f8440f415a8e685
Charles Painter <cpainter85@gmail.com>
2015-03-25 09:32:50 -0600

    initial tests

---
+++ spec/recipe_converter_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../recipe_converter'
+
+RSpec.describe RecipeConverter do
+  let(:list_of_ingredients) {[{salt: 480}, {oil: 720} ]}
+  let(:ingredients) { RecipeConverter.new(:list_of_ingredients)}
+
+  describe '#to_tablespoons' do
+    it 'returns list of ingredients with amounts in tablespoons' do
+      ingredients_in_tablespoons = ingredients.to_tablespoons
+      expected_output = [{salt: 32}, {oil: 48}]
+      expect(ingredients_in_tablespoons).to eq(expected_output)
+    end
+  end
+
+  describe '#to_cups' do
+    it 'returns list of ingredients with amounts in cups' do
+      ingredients_in_cups = ingredients.to_cups
+      expected_output = [{salt: 2}, {oil: 3}]
+      expect(ingredients_in_cups).to eq(expected_output)
+    end
+  end
+end
